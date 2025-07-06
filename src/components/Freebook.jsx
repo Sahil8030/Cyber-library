408d74089a183edfd75c429da386c5042163a5bb
Sahil Mahajan <saahilmahajan218@gmail.com>
2025-07-06 22:19:32 +0530

    Update Freebook.jsx

--- src/components/Freebook.jsx
+++ src/components/Freebook.jsx
@@ -10,31 +10,31 @@ function Freebook() {
     {
       id: 1,
       title: "Grey hat",
-      image: "/image.png",
+      image: "./public/image.png",
       link: "https://github.com/DoS0x99/cyber-security-books/blob/main/CyberSec%20In%20General/Gray%20Hat%20Hacking.pdf",
     },
     {
       id: 2,
       title: "Advanced persistent hacking",
-      image: "/advance.png",
+      image: "./public/advance.png",
       link: "https://github.com/DoS0x99/cyber-security-books/blob/main/CyberSec%20In%20General/Advanced%20Persistent%20Threat%20Hacking.pdf",
     },
     {
       id: 3,
       title: "Penetration testing Windows",
-      image: "/pent.png",
+      image: "./public/pent.png",
       link: "https://github.com/DoS0x99/cyber-security-books/blob/main/CyberSec%20In%20General/Hands-On%20Penetration%20Testing%20on%20Windows.pdf",
     },
     {
       id: 4,
       title: "Linux",
-      image: "/linux.png",
+      image: "./public/linux.png",
       link: "https://github.com/DoS0x99/cyber-security-books/blob/main/CyberSec%20In%20General/Linux%20Firewalls.pdf",
     },
     {
       id: 5,
       title: "Social engineering",
-      image: "/social.png",
+      image: "./public/social.png",
       link: "https://github.com/DoS0x99/cyber-security-books/blob/main/CyberSec%20In%20General/Social%20Engineering%20-%20The%20Art%20of%20Human%20Hacking.pdf",
     },
   ];
