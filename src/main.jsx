616f566dc05cfcfa2f7ff48bdf8049aa8dc01a32
Sahil Mahajan <saahilmahajan218@gmail.com>
2025-07-06 21:32:12 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,16 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
-import AuthProvider from "./context/AuthProvider.jsx";
+
+// ✅ Correct named import
+import { AuthProvider } from "./context/AuthProvider.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <AuthProvider>
-      <div className="dark:bg-slate-900 dark:text-white">
+  <React.StrictMode>
+    <BrowserRouter>
+      <AuthProvider>
         <App />
-      </div>
-    </AuthProvider>
-  </BrowserRouter>
+      </AuthProvider>
+    </BrowserRouter>
+  </React.StrictMode>
 );
