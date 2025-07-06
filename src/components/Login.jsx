b91018f0bf7ee8d88a2b033ae3b42fe4b5dec8ca
Sahil Mahajan <saahilmahajan218@gmail.com>
2025-07-06 23:36:33 +0530

    Update Login.jsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,94 +1,111 @@
 import React from "react";
-import Slider from "react-slick";
+import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import axios from "axios";
+import toast from "react-hot-toast";
+function Login() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
 
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-
-import Cards from "./Cards";
-
-function Freebook() {
-  const book = [
-    {
-      id: 1,
-      title: "Grey hat",
-      image: "/image.png",
-      link: "https://github.com/DoS0x99/cyber-security-books/blob/main/CyberSec%20In%20General/Gray%20Hat%20Hacking.pdf",
-    },
-    {
-      id: 2,
-      title: "Advanced persistant hacking",
-      image: "/advance.png",
-      link: "https://github.com/DoS0x99/cyber-security-books/blob/main/CyberSec%20In%20General/Advanced%20Persistent%20Threat%20Hacking.pdf",
-    },
-    {
-      id: 3,
-      title: "Pentration testing Windows",
-      image: "pent.png",
-      link: "https://github.com/DoS0x99/cyber-security-books/blob/main/CyberSec%20In%20General/Hands-On%20Penetration%20Testing%20on%20Windows.pdf",
-    },
-    {
-      id: 4,
-      title: "Linux",
-      image: "/linux.png",
-      link: "https://github.com/DoS0x99/cyber-security-books/blob/main/CyberSec%20In%20General/Linux%20Firewalls.pdf",
-    },
-    {
-      id: 5,
-      title: "Social engineering",
-      image: "/social.png",
-      link: "https://github.com/DoS0x99/cyber-security-books/blob/main/CyberSec%20In%20General/Social%20Engineering%20-%20The%20Art%20of%20Human%20Hacking.pdf",
-    },
-  ];
-
-  const settings = {
-    dots: true,
-    infinite: false,
-    speed: 500,
-    slidesToShow: 3,
-    slidesToScroll: 3,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-        },
-      },
-      {
-        breakpoint: 768,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-        },
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-        },
-      },
-    ],
+  const onSubmit = async (data) => {
+    const userInfo = {
+      email: data.email,
+      password: data.password,
+    };
+    await axios
+      .post("http://localhost:4001/user/login", userInfo)
+      .then((res) => {
+        console.log(res.data);
+        if (res.data) {
+          toast.success("Loggedin Successfully");
+          document.getElementById("my_modal_3").close();
+          setTimeout(() => {
+            window.location.reload();
+            localStorage.setItem("Users", JSON.stringify(res.data.user));
+          }, 1000);
+        }
+      })
+      .catch((err) => {
+        if (err.response) {
+          console.log(err);
+          toast.error("Error: " + err.response.data.message);
+          setTimeout(() => {}, 2000);
+        }
+      });
   };
-
   return (
-    <div className="max-w-screen-2xl container mx-auto md:px-20 px-4 mt-10">
-      <div>
-        <h1 className="font-semibold text-2xl pb-2">Free Books</h1>
-        <p className="text-grey-900">
-          Start learning without limits. Explore our handpicked cybersecurity books — absolutely free, forever. No cost. No catch. Just pure knowledge to fuel your journey.
-        </p>
-      </div>
+    <div>
+      <dialog id="my_modal_3" className="modal">
+        <div className="modal-box">
+          <form onSubmit={handleSubmit(onSubmit)} method="dialog">
+            {/* if there is a button in form, it will close the modal */}
+            <Link
+              to="/"
+              className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2"
+              onClick={() => document.getElementById("my_modal_3").close()}
+            >
+              ✕
+            </Link>
 
-      <div className="mt-10">
-        <Slider {...settings}>
-          {book.map((item) => (
-            <Cards item={item} key={item.id} />
-          ))}
-        </Slider>
-      </div>
+            <h3 className="font-bold text-lg">Login</h3>
+            {/* Email */}
+            <div className="mt-4 space-y-2">
+              <span>Email</span>
+              <br />
+              <input
+                type="email"
+                placeholder="Enter your email"
+                className="w-80 px-3 py-1 border rounded-md outline-none"
+                {...register("email", { required: true })}
+              />
+              <br />
+              {errors.email && (
+                <span className="text-sm text-red-500">
+                  This field is required
+                </span>
+              )}
+            </div>
+            {/* password */}
+            <div className="mt-4 space-y-2">
+              <span>Password</span>
+              <br />
+              <input
+                type="password"
+                placeholder="Enter your password"
+                className="w-80 px-3 py-1 border rounded-md outline-none"
+                {...register("password", { required: true })}
+              />
+              <br />
+              {errors.password && (
+                <span className="text-sm text-red-500">
+                  This field is required
+                </span>
+              )}
+            </div>
+
+            {/* Button */}
+            <div className="flex justify-around mt-6">
+              <button className="bg-pink-500 text-white rounded-md px-3 py-1 hover:bg-pink-700 duration-200">
+                Login
+              </button>
+              <p>
+                Not registered?{" "}
+                <Link
+                  to="/signup"
+                  className="underline text-blue-500 cursor-pointer"
+                >
+                  Signup
+                </Link>{" "}
+              </p>
+            </div>
+          </form>
+        </div>
+      </dialog>
     </div>
   );
 }
 
-export default Freebook;
+export default Login;
